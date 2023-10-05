5f4c998aaf400c1d621389260295388e91976eab
CianMurray98 <cianmurraycian@gmail.com>
2023-10-05 15:29:07 +0100

    first commit and push

--- src/main/java/ie/atu/helloworldspringboot/HelloWorldSpringBootApplication.java
+++ src/main/java/ie/atu/helloworldspringboot/HelloWorldSpringBootApplication.java
@@ -11,5 +11,6 @@ public class HelloWorldSpringBootApplication {
         SpringApplication.run(HelloWorldSpringBootApplication.class, args);
     }{
         System.out.println("Hello, World!");
+
     }
 }
