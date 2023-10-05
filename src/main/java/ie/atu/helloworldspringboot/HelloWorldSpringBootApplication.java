00ac447e67bc8b079f6c1cbaa81fded6c49dad5f
CianMurray98 <cianmurraycian@gmail.com>
2023-10-05 15:11:19 +0100

    first commit and push

---
+++ src/main/java/ie/atu/helloworldspringboot/HelloWorldSpringBootApplication.java
@@ -0,0 +1,15 @@
+package ie.atu.helloworldspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HelloWorldSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+    }{
+        System.out.println("Hello, World!");
+    }
+}
